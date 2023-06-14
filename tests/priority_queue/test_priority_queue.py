0566e4ee86b74f01fa6634c513b8075d7d287803
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-06-14 11:34:58 -0300

    feat:requisito-06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,24 @@
-# from ting_file_management.priority_queue import PriorityQueue
+import pytest
+from ting_file_management.priority_queue import PriorityQueue
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+
+    priority_queue.enqueue({"qtd_linhas": 1})
+    priority_queue.enqueue({"qtd_linhas": 9})
+    priority_queue.enqueue({"qtd_linhas": 2})
+    priority_queue.enqueue({"qtd_linhas": 7})
+
+    assert len(priority_queue) == 4
+    assert priority_queue.search(1) == {"qtd_linhas": 2}
+
+    given = priority_queue.dequeue()
+    assert len(priority_queue) == 3
+    assert given == ({"qtd_linhas": 1})
+
+    given_1 = priority_queue.dequeue()
+    assert len(priority_queue) == 2
+    assert given_1 == ({"qtd_linhas": 2})
+    with pytest.raises(IndexError):
+        priority_queue.search(10)
