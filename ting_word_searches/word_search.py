33c3402d2dc08b96c1c42da65a229a2064774ad0
Luiz Gustavo Ferreira <gus.116506@gmail.com>
2023-06-14 16:04:15 -0300

    feat:requisito-07-08

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,51 @@
+# from ting_file_management.file_management import txt_importer
+from ting_file_management.queue import Queue
+
+queue_instance = Queue()
+text_file = "statics/arquivo_teste.txt"
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    counter = 0
+    result = []
+
+    for i in range(len(instance)):
+        fila = instance.search(i)
+        instance_result = {
+            "palavra": word,
+            "arquivo": fila["nome_do_arquivo"],
+            "ocorrencias": [{
+                "linha": index + 1
+                } for index, linha in enumerate(fila["linhas_do_arquivo"])
+                        if word.lower() in linha.lower()]
+        }
+        result.append(instance_result)
+        if len(instance_result["ocorrencias"]) == 0:
+            counter += 1
+    if counter == len(instance):
+        return []
+
+    return result
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    counter = 0
+    result = []
+
+    for i in range(len(instance)):
+        fila = instance.search(i)
+        instance_result = {
+            "palavra": word,
+            "arquivo": fila["nome_do_arquivo"],
+            "ocorrencias": [{
+                "linha": index + 1, "conteudo": linha
+                } for index, linha in enumerate(fila["linhas_do_arquivo"])
+                        if word.lower() in linha.lower()]
+        }
+        result.append(instance_result)
+        if len(instance_result["ocorrencias"]) == 0:
+            counter += 1
+    if counter == len(instance):
+        return []
+
+    return result
